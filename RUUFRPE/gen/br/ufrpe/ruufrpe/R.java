f0aaadcd61ee412b4ef7e424c8ec865afa10b0eb
Lucas Maia <ldmaia92@hotmail.com>
2013-12-05 17:46:37 -0200

    a

--- RUUFRPE/gen/br/ufrpe/ruufrpe/R.java
+++ RUUFRPE/gen/br/ufrpe/ruufrpe/R.java
@@ -22,6 +22,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ufrpe=0x7f020001;
+        public static final int ufrpeicone=0x7f020002;
     }
     public static final class id {
         public static final int Almoco=0x7f080001;
@@ -50,6 +52,7 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int row=0x7f030001;
+        public static final int splashscreen=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
